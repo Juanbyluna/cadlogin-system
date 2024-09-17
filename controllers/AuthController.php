741f456586706668bc915e3e3fce939ffca2abe4
juanbyluna <juanfernandesdegoes@gmail.com>
2024-09-17 08:32:46 -0300

    cria código no site(base)

---
+++ controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php 
+// requer o qrquivo user que contém o model user com as funções de manipulação de dados de usuários 
+require_once 'models/user.php'
+
+class AuthController
+{
+    // função responsável processo de login
+    public function login ()
+    {
+        //verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
+        
+    }
+}
+
+?>
