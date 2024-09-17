4fb55bb34e519790fabd9afd928e599f3a45e619
juanbyluna <juanfernandesdegoes@gmail.com>
2024-09-17 08:31:59 -0300

    finalização do reutes

--- routes.php
+++ routes.php
@@ -1,7 +1,20 @@
 <?php 
 //inclui arquivos de controlador necessarios para lidar com diferentes ações
- require 'controllers/AutheController.php'; //inclui o controlador de autentocação
- require 'controllers/UserCpontroller.php'; //inclui o controlador de usuários
+ require 'controllers/AuthController.php'; //inclui o controlador de autentocação
+ require 'controllers/UserController.php'; //inclui o controlador de usuários
  require 'controllers/DashboardController.php'; //inclui o controllador de dashboard
+
+ // Cria instancias dos controladores para utilizar seus métodos 
+ $authController = new AuthController(); //instancia controlador de autenticação
+ $userController = new UserController(); //instancia controlador de dashboard
+ $dashboardController = new  DashboardController(); //instancia controlador de dashboard
+
+ // Coleta a ação de URL, se não houver ação definida, usa 'login' como padrão
+ $action = $_GET['action'] ?? 'login'; // usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente 
+
+ switch ($action){
+    case 'login':
+        $authController->login(); //chama o método de login do controlador de autenticação[]
+ }
  
 ?>
