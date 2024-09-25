9d5c1a0e8aebd038dca6e5f24b2f73a3efd6e047
juanbyluna <juanfernandesdegoes@gmail.com>
2024-09-25 08:19:08 -0300

    finaliza projeto e começa a estilização do projeto em geral e inicio da estilização

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,15 +1,27 @@
-<?php 
-// requer o qrquivo user que contém o model user com as funções de manipulação de dados de usuários 
-require_once 'models/user.php'
-
+<?php
+//requer o arquivo user que contém o model user com as fubções de manipulaão de dados de usuários
+require_once 'models/user.php';
+ 
 class AuthController
 {
-    // função responsável processo de login
-    public function login ()
+    //função responsável responsável para realizar login
+    public function login()
     {
-        //verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
-        
+        //Verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // obter os valores do fomrulário
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+ 
+            // chama o métdodo do model para encontrar usuário pelo email
+            $user = User::findByEmail();
+           
+            if($user && password_verify(4senha, $user['senha'])){ // Verifica se a senha corresponde a um hash
+                session_start();
+                // armazena na sessão o ID do usuário que está logado e seu perfil
+                $_SESSION['usuario_id'] = $user ['id'];
+            }
+        }
     }
 }
-
 ?>
